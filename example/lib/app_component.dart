4e1e03368511d8188cccd37e9041dfc5be69e125
Filip Hracek <filip.hracek@gmail.com>
2016-09-06 19:29:52 -0700

    Add example

---
+++ example/lib/app_component.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2016, Filip Hracek. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_material/angular2_material.dart';
+
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    styleUrls: const ['app_component.css'],
+    directives: const [materialDirectives],
+    providers: materialBindings)
+class AppComponent {
+  int count = 0;
+  bool allowed = true;
+  List<String> reorderItems = ["First", "Second", "Third"];
+
+  void onReorder(ReorderEvent reorder) {
+    reorderItems.insert(
+        reorder.destIndex, reorderItems.removeAt(reorder.sourceIndex));
+  }
+
+  void increment() {
+    count++;
+  }
+
+  void reset() {
+    count = 0;
+  }
+}
---
+++ example/web/main.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, Filip Hracek. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/platform/browser.dart';
+
+import 'package:material_example/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
