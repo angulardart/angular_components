8484de4b0a064d9efbe7cd50bae48af0ac86b39c
Nicholas Shahan <nshahan@google.com>
2017-04-21 10:25:23 -0700

    Remove all files and export new package
    Depricate angular2_components library. Update README to alert users to the change.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+> NOTE: This package has been renamed to angular_components without the "2". 
+> To continue to receive the latest versions update your `pubspec.yaml` to 
+> depend on `angular_components: ^0.5.0-beta`. 
+
 ## Preview Release
 
 These are the AngularDart components that Google uses to build many of its web
--- lib/angular2_components.dart
+++ lib/angular2_components.dart
@@ -2,69 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+
+// This package has been renamed to angular_components without the "2".
+// To continue to receive the latest versions update your pubspec.yaml to
+// depend on angular_components: ^0.5.0-beta.
+@deprecated
 library angular2_components;
 
-import 'package:angular2/angular2.dart' show Provider;
-import 'src/all_components.dart';
-export 'src/all_components.dart';
-
-/// A convenience list of all Directives exposed by this package.
-const List<Type> materialDirectives = const [
-  AutoDismissDirective,
-  AutoFocusDirective,
-  CachingDeferredContentDirective,
-  ClickableTooltipTargetDirective,
-  DarkThemeDirective,
-  DeferredContentDirective,
-  DisplayNameRendererDirective,
-  FixedMaterialTabStripComponent,
-  FocusActivableItemDirective,
-  FocusItemDirective,
-  FocusListDirective,
-  FocusableDirective,
-  FocusTrapComponent,
-  GlyphComponent,
-  MaterialButtonComponent,
-  MaterialCheckboxComponent,
-  MaterialChipComponent,
-  MaterialChipsComponent,
-  MaterialDialogComponent,
-  MaterialDropdownSelectComponent,
-  MaterialExpansionPanel,
-  MaterialExpansionPanelSet,
-  MaterialFabComponent,
-  MaterialIconTooltipComponent,
-  MaterialInkTooltipComponent,
-  MaterialInputComponent,
-  MaterialInputDefaultValueAccessor,
-  MaterialListComponent,
-  MaterialListItemComponent,
-  MaterialMultilineInputComponent,
-  MaterialPaperTooltipComponent,
-  MaterialPopupComponent,
-  MaterialProgressComponent,
-  MaterialRadioComponent,
-  MaterialRadioGroupComponent,
-  MaterialRippleComponent,
-  MaterialSelectComponent,
-  MaterialSelectItemComponent,
-  MaterialSpinnerComponent,
-  MaterialTabComponent,
-  MaterialTabPanelComponent,
-  MaterialToggleComponent,
-  MaterialTooltipDirective,
-  MaterialTooltipSourceDirective,
-  MaterialTooltipTargetDirective,
-  MaterialYesNoButtonsComponent,
-  ModalComponent,
-  NgModel,
-  PopupSourceDirective,
-  ReorderItemDirective,
-  ReorderListComponent,
-  ScorecardComponent,
-];
-
-/// A convenience list of all providers exposed by this package.
-const List<List<Provider>> materialProviders = const <List<Provider>>[
-  popupBindings,
-];
+export 'package:angular_components/angular_components.dart';
