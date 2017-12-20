5100c4cfae2ae9b66abaf129f01cc73d8c792602
nshahan <nshahan@google.com>
2017-12-20 13:59:21 -0800

    Clarify file comment about angular dependency.
    Based on feedback from external sync
    https://github.com/dart-lang/angular_components/pull/203
    
    PiperOrigin-RevId: 179729861

--- lib/model/ui/has_factory.dart
+++ lib/model/ui/has_factory.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// This file is separate from has_renderer so it doesn't add in the angular
-// dependency and so 'dart:html' which stops tests from running as a simple
+// This file is separate from has_renderer.dart so it doesn't add the angular
+// dependency and transitively 'dart:html', which doesn't work in a simple
 // dart_test.
 import 'package:angular/angular.dart' show ComponentFactory;
 
@@ -11,8 +11,8 @@ import 'has_renderer.dart';
 
 export 'has_renderer.dart';
 
-/// Defines a method that returns a factory to render the Item.  The
-/// component created must implement [RendersValue].
+/// Defines a method that returns a factory to render the Item.  The component
+/// created must implement [RendersValue].
 typedef ComponentFactory FactoryRenderer<T extends RendersValue, I>(I item);
 
 /// HasFactoryRenderer defines a method that takes in an item and returns the
