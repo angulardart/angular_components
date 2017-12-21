774f68e06650a6badf5fce44bc0c86d2a0a69f63
tsander <tsander@google.com>
2017-12-20 17:53:58 -0800

    Change componentFactory, and componentType to accept the right type. Move the initialization of the component into ngChanges so that if the componentFactory, and the value both change in the same turn the component will be initialized with the correct value right away.
    PiperOrigin-RevId: 179756891

--- lib/dynamic_component/dynamic_component.dart
+++ lib/dynamic_component/dynamic_component.dart
@@ -23,7 +23,7 @@ import 'package:angular_components/model/ui/has_renderer.dart';
   selector: 'dynamic-component',
   template: '''<template #marker></template>''',
 )
-class DynamicComponent implements OnDestroy {
+class DynamicComponent implements OnDestroy, AfterChanges {
   final SlowComponentLoader _slowComponentLoader;
   final ComponentLoader _componentLoader;
   final ChangeDetectorRef _changeDetectorRef;
@@ -43,8 +43,11 @@ class DynamicComponent implements OnDestroy {
 
   ComponentRef _childComponent;
   Type _componentType;
+  bool _typeChanged = false;
   ComponentFactory _componentFactory;
+  bool _factoryChanged = false;
   Object _value;
+  bool _valueChanged = false;
 
   /// Fired when component is loaded allowing clients to get a handle on the
   /// component loaded.
@@ -68,48 +71,44 @@ class DynamicComponent implements OnDestroy {
     _childComponent = null;
   }
 
-  void _checkAndInitialize() {
-    _disposeChildComponent();
-    if (_viewContainerRef != null) {
-      _initialize();
-    } else {
-      _loadDeferred = true;
-    }
+  /// The type of component to dynamically render.
+  @Deprecated('Use componentFactory instead as it is more tree-shakable')
+  @Input()
+  set componentType(Type dartType) {
+    if (_componentType != dartType) _typeChanged = true;
+    _componentType = dartType;
   }
 
   /// The type of component to dynamically render.
-  ///
-  /// If something other than a null or Type is passed in, it will
-  /// be ignored.
-  @Deprecated('Use componentFactory instead as it is more tree-shakable')
   @Input()
-  set componentType(Object dartType) {
-    // Ignore unsupported type.
-    if (dartType == null) {
-      _disposeChildComponent();
-    } else if (dartType is Type) {
-      _componentFactory = null;
-      _componentType = dartType;
-      _checkAndInitialize();
-    }
+  set componentFactory(ComponentFactory component) {
+    if (_componentFactory != component) _factoryChanged = true;
+    _componentFactory = component;
   }
 
-  /// The ComponentFactory of component to dynamically render.
-  ///
-  /// If something other than a null or ComponentFactory is passed in, it will
-  /// be ignored. Note: This only temporarially supports Object to allow for
-  /// an easier transition of code. This does make this code slightly slower,
-  /// but will be changed to [ComponentFactory] when [componentType] is
-  /// removed.
+  /// The value to set on the component if the component implements
+  /// [RendersValue]. Optional.
   @Input()
-  set componentFactory(Object component) {
-    if (component == null) {
+  set value(dynamic s) {
+    _value = s;
+    _valueChanged = true;
+  }
+
+  @override
+  void ngAfterChanges() {
+    if (_factoryChanged || _typeChanged) {
       _disposeChildComponent();
-    } else if (component is ComponentFactory) {
-      _componentType = null;
-      _componentFactory = component;
-      _checkAndInitialize();
+      if (_viewContainerRef != null) {
+        _initialize();
+      } else {
+        _loadDeferred = true;
+      }
+    } else if (_valueChanged) {
+      // Only update the child if the component was not changed. If the
+      // component was changed then it will get initialized with the value.
+      _updateChildComponent();
     }
+    _valueChanged = _factoryChanged = _typeChanged = false;
   }
 
   void _initialize() {
@@ -122,7 +121,7 @@ class DynamicComponent implements OnDestroy {
           _componentFactory, _viewContainerRef);
       _onLoadController.add(_childComponent);
       _updateChildComponent();
-    } else {
+    } else if (_componentType != null) {
       // TODO(google): Remove this code once componentType is no longer used.
       Type loadType = _componentType;
       _slowComponentLoader
@@ -142,14 +141,6 @@ class DynamicComponent implements OnDestroy {
         _updateChildComponent();
       });
     }
-  }
-
-  /// The value to set on the component if the component implements
-  /// [RendersValue]. Optional.
-  @Input()
-  set value(dynamic s) {
-    _value = s;
-    _updateChildComponent();
   }
 
   void _updateChildComponent() {
