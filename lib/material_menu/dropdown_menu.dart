904c3ca08bf4bd7112aadbd408330ef1313145dc
rkj <rkj@google.com>
2018-03-26 22:35:46 -0700

    Remove visibility.all from @Component annotations. This is potentially BREAKING change.
    Please review carefully. Breakage will only be caught if the code is covered by
    tests. Reject this change if you believe it breaks you.
    
    If the component needs to be injected anywhere in the app, you can get a
    runtime exception after this change. This should be caught by tests using your
    components, and fixing the issue should be trivial, as the exceptions have good
    error messages, such as "No provider found for FooComponent".
    
    LSC Doc:
    https://docs.google.com/document/d/1aoVWQANsQOpmAR7-ldfLYg1-Z0LifFfHv4pVmOc8cD4/edit
    
    #angular #visibility
    
    Tested:
        tap_presubmit: http://test/OCL:189916950:BASE:190481603:1522109070742:a06a5f5d
        Some tests failed; test failures are believed to be unrelated to this CL
    PiperOrigin-RevId: 190574208

--- lib/material_menu/dropdown_menu.dart
+++ lib/material_menu/dropdown_menu.dart
@@ -24,8 +24,6 @@ import 'package:angular_components/utils/disposer/disposer.dart';
   changeDetection: ChangeDetectionStrategy.OnPush,
   // TODO(google): Change preserveWhitespace to false to improve codesize.
   preserveWhitespace: true,
-  // TODO(google): Change to `Visibility.local` to reduce code size.
-  visibility: Visibility.all,
 )
 class DropdownMenuComponent extends Object
     with FocusableMixin, MenuPopupWrapper
