cdfcb168c66a40a72dcdc7d0694c3ccc90c8aad5
davidmorgan <davidmorgan@google.com>
2018-07-12 00:45:09 -0700

    Fix violations of prefer_is_not_empty
    PiperOrigin-RevId: 204255881

--- lib/mixins/button_wrapper.dart
+++ lib/mixins/button_wrapper.dart
@@ -38,7 +38,7 @@ class MaterialButtonWrapper implements HasDisabled {
   Icon _buttonIcon;
   Icon get buttonIcon {
     if (icon != null) return icon;
-    if (_buttonIcon == null && (iconName != null && !iconName.isEmpty)) {
+    if (_buttonIcon == null && (iconName != null && iconName.isNotEmpty)) {
       _buttonIcon = new Icon(iconName);
     }
     return _buttonIcon;
