d42b6fdf309ba83bf6532f5ddebc0986b51ced7e
Nicholas Shahan <nshahan@google.com>
2018-08-21 17:00:52 -0700

    add protoc to PATH

--- angular_components/tool/travis/install_protoc.sh
+++ angular_components/tool/travis/install_protoc.sh
@@ -12,5 +12,8 @@ wget https://github.com/google/protobuf/releases/download/v$PROTOC_VERSION/proto
 unzip protoc-$PROTOC_VERSION-linux-x86_64.zip
 popd
 
+PATH=$HOME/protoc/bin:$PATH
+protoc --version
+
 # Activate dart protoc plugin.
 pub global activate protoc_plugin
