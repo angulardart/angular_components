69bf87fcafa4b5e90d4fb6e8753dca7b5e22c511
Nicholas Shahan <nshahan@google.com>
2018-09-20 16:59:58 -0700

    Updated outputs

--- angular_components/lib/material_datepicker/proto/date.pb.dart
+++ angular_components/lib/material_datepicker/proto/date.pb.dart
@@ -1,37 +1,40 @@
 ///
 //  Generated code. Do not modify.
+//  source: date.proto
 ///
-// ignore_for_file: non_constant_identifier_names,library_prefixes
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
 
 // ignore: UNUSED_SHOWN_NAME
 import 'dart:core' show int, bool, double, String, List, override;
 
-import 'package:protobuf/protobuf.dart';
+import 'package:protobuf/protobuf.dart' as $pb;
 
-class Date extends GeneratedMessage {
-  static final BuilderInfo _i = new BuilderInfo('Date')
-    ..a<int>(1, 'year', PbFieldType.O3)
-    ..a<int>(2, 'month', PbFieldType.O3)
-    ..a<int>(3, 'day', PbFieldType.O3)
+class Date extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = new $pb.BuilderInfo('Date',
+      package: const $pb.PackageName(
+          'angular_components.lib.material_datepicker.proto'))
+    ..a<int>(1, 'year', $pb.PbFieldType.O3)
+    ..a<int>(2, 'month', $pb.PbFieldType.O3)
+    ..a<int>(3, 'day', $pb.PbFieldType.O3)
     ..hasRequiredFields = false;
 
   Date() : super();
-  Date.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+  Date.fromBuffer(List<int> i,
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromBuffer(i, r);
-  Date.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+  Date.fromJson(String i,
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromJson(i, r);
   Date clone() => new Date()..mergeFromMessage(this);
-  BuilderInfo get info_ => _i;
+  Date copyWith(void Function(Date) updates) =>
+      super.copyWith((message) => updates(message as Date));
+  $pb.BuilderInfo get info_ => _i;
   static Date create() => new Date();
-  static PbList<Date> createRepeated() => new PbList<Date>();
-  static Date getDefault() {
-    if (_defaultInstance == null) _defaultInstance = new _ReadonlyDate();
-    return _defaultInstance;
-  }
-
+  static $pb.PbList<Date> createRepeated() => new $pb.PbList<Date>();
+  static Date getDefault() => _defaultInstance ??= create()..freeze();
   static Date _defaultInstance;
   static void $checkItem(Date v) {
-    if (v is! Date) checkItemFailed(v, 'Date');
+    if (v is! Date) $pb.checkItemFailed(v, _i.messageName);
   }
 
   int get year => $_get(0, 0);
@@ -58,5 +61,3 @@ class Date extends GeneratedMessage {
   bool hasDay() => $_has(2);
   void clearDay() => clearField(3);
 }
-
-class _ReadonlyDate extends Date with ReadonlyMessageMixin {}
--- angular_components/lib/material_datepicker/proto/date_range.pb.dart
+++ angular_components/lib/material_datepicker/proto/date_range.pb.dart
@@ -1,53 +1,54 @@
 ///
 //  Generated code. Do not modify.
+//  source: date_range.proto
 ///
-// ignore_for_file: non_constant_identifier_names,library_prefixes
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
 
 // ignore: UNUSED_SHOWN_NAME
 import 'dart:core' show int, bool, double, String, List, override;
 
-import 'package:protobuf/protobuf.dart';
+import 'package:protobuf/protobuf.dart' as $pb;
 
-import 'date.pb.dart';
+import 'date.pb.dart' as $0;
 
-class DatepickerDateRange extends GeneratedMessage {
-  static final BuilderInfo _i = new BuilderInfo('DatepickerDateRange')
-    ..a<DateRange>(
-        1, 'dateRange', PbFieldType.OM, DateRange.getDefault, DateRange.create)
-    ..a<int>(2, 'daysAgo', PbFieldType.O3)
-    ..a<int>(3, 'weeksAgo', PbFieldType.O3)
-    ..a<int>(4, 'monthsAgo', PbFieldType.O3)
-    ..a<int>(5, 'yearsAgo', PbFieldType.O3)
-    ..a<int>(6, 'lastNDays', PbFieldType.O3)
+class DatepickerDateRange extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = new $pb.BuilderInfo('DatepickerDateRange',
+      package: const $pb.PackageName(
+          'angular_components.lib.material_datepicker.proto'))
+    ..a<DateRange>(1, 'dateRange', $pb.PbFieldType.OM, DateRange.getDefault,
+        DateRange.create)
+    ..a<int>(2, 'daysAgo', $pb.PbFieldType.O3)
+    ..a<int>(3, 'weeksAgo', $pb.PbFieldType.O3)
+    ..a<int>(4, 'monthsAgo', $pb.PbFieldType.O3)
+    ..a<int>(5, 'yearsAgo', $pb.PbFieldType.O3)
+    ..a<int>(6, 'lastNDays', $pb.PbFieldType.O3)
     ..aOB(7, 'allTimeRange')
-    ..a<int>(8, 'broadcastMonthsAgo', PbFieldType.O3)
-    ..a<int>(9, 'lastNDaysToToday', PbFieldType.O3)
-    ..a<int>(10, 'quartersAgo', PbFieldType.O3)
-    ..a<int>(11, 'startWeekday', PbFieldType.O3)
+    ..a<int>(8, 'broadcastMonthsAgo', $pb.PbFieldType.O3)
+    ..a<int>(9, 'lastNDaysToToday', $pb.PbFieldType.O3)
+    ..a<int>(10, 'quartersAgo', $pb.PbFieldType.O3)
+    ..a<int>(11, 'startWeekday', $pb.PbFieldType.O3)
     ..hasRequiredFields = false;
 
   DatepickerDateRange() : super();
   DatepickerDateRange.fromBuffer(List<int> i,
-      [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromBuffer(i, r);
   DatepickerDateRange.fromJson(String i,
-      [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromJson(i, r);
   DatepickerDateRange clone() =>
       new DatepickerDateRange()..mergeFromMessage(this);
-  BuilderInfo get info_ => _i;
+  DatepickerDateRange copyWith(void Function(DatepickerDateRange) updates) =>
+      super.copyWith((message) => updates(message as DatepickerDateRange));
+  $pb.BuilderInfo get info_ => _i;
   static DatepickerDateRange create() => new DatepickerDateRange();
-  static PbList<DatepickerDateRange> createRepeated() =>
-      new PbList<DatepickerDateRange>();
-  static DatepickerDateRange getDefault() {
-    if (_defaultInstance == null)
-      _defaultInstance = new _ReadonlyDatepickerDateRange();
-    return _defaultInstance;
-  }
-
+  static $pb.PbList<DatepickerDateRange> createRepeated() =>
+      new $pb.PbList<DatepickerDateRange>();
+  static DatepickerDateRange getDefault() =>
+      _defaultInstance ??= create()..freeze();
   static DatepickerDateRange _defaultInstance;
   static void $checkItem(DatepickerDateRange v) {
-    if (v is! DatepickerDateRange) checkItemFailed(v, 'DatepickerDateRange');
+    if (v is! DatepickerDateRange) $pb.checkItemFailed(v, _i.messageName);
   }
 
   DateRange get dateRange => $_getN(0);
@@ -139,50 +140,48 @@ class DatepickerDateRange extends GeneratedMessage {
   void clearStartWeekday() => clearField(11);
 }
 
-class _ReadonlyDatepickerDateRange extends DatepickerDateRange
-    with ReadonlyMessageMixin {}
-
-class DateRange extends GeneratedMessage {
-  static final BuilderInfo _i = new BuilderInfo('DateRange')
-    ..a<Date>(1, 'start', PbFieldType.OM, Date.getDefault, Date.create)
-    ..a<Date>(2, 'end', PbFieldType.OM, Date.getDefault, Date.create)
+class DateRange extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = new $pb.BuilderInfo('DateRange',
+      package: const $pb.PackageName(
+          'angular_components.lib.material_datepicker.proto'))
+    ..a<$0.Date>(
+        1, 'start', $pb.PbFieldType.OM, $0.Date.getDefault, $0.Date.create)
+    ..a<$0.Date>(
+        2, 'end', $pb.PbFieldType.OM, $0.Date.getDefault, $0.Date.create)
     ..hasRequiredFields = false;
 
   DateRange() : super();
   DateRange.fromBuffer(List<int> i,
-      [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromBuffer(i, r);
-  DateRange.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+  DateRange.fromJson(String i,
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromJson(i, r);
   DateRange clone() => new DateRange()..mergeFromMessage(this);
-  BuilderInfo get info_ => _i;
+  DateRange copyWith(void Function(DateRange) updates) =>
+      super.copyWith((message) => updates(message as DateRange));
+  $pb.BuilderInfo get info_ => _i;
   static DateRange create() => new DateRange();
-  static PbList<DateRange> createRepeated() => new PbList<DateRange>();
-  static DateRange getDefault() {
-    if (_defaultInstance == null) _defaultInstance = new _ReadonlyDateRange();
-    return _defaultInstance;
-  }
-
+  static $pb.PbList<DateRange> createRepeated() => new $pb.PbList<DateRange>();
+  static DateRange getDefault() => _defaultInstance ??= create()..freeze();
   static DateRange _defaultInstance;
   static void $checkItem(DateRange v) {
-    if (v is! DateRange) checkItemFailed(v, 'DateRange');
+    if (v is! DateRange) $pb.checkItemFailed(v, _i.messageName);
   }
 
-  Date get start => $_getN(0);
-  set start(Date v) {
+  $0.Date get start => $_getN(0);
+  set start($0.Date v) {
     setField(1, v);
   }
 
   bool hasStart() => $_has(0);
   void clearStart() => clearField(1);
 
-  Date get end => $_getN(1);
-  set end(Date v) {
+  $0.Date get end => $_getN(1);
+  set end($0.Date v) {
     setField(2, v);
   }
 
   bool hasEnd() => $_has(1);
   void clearEnd() => clearField(2);
 }
-
-class _ReadonlyDateRange extends DateRange with ReadonlyMessageMixin {}
