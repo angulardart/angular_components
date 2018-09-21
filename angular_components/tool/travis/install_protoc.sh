d3dbc42552eebafdab538143d4b872cfb894412a
Nicholas Shahan <nshahan@google.com>
2018-09-21 11:22:05 -0700

    Add missing trailing new lines

--- angular_components/tool/travis/install_protoc.sh
+++ angular_components/tool/travis/install_protoc.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Only run externally during presubmits on Travis.
-PROTOC_PLUGIN_VERSION=0.10.2
-PROTOC_VERSION=3.6.0
+PROTOC_PLUGIN_VERSION="0.10.2"
+PROTOC_VERSION="3.6.0"
 
 
 if type protoc > /dev/null; then
   echo "protoc already installed."
 
   # Activate dart protoc plugin.
-  pub global activate protoc_plugin "$PROTOC_PLUGIN_VERSION"
+  pub global activate protoc_plugin $PROTOC_PLUGIN_VERSION
   exit 0
 fi
 
@@ -27,4 +27,4 @@ popd
 $HOME/protoc/bin/protoc --version
 
 # Activate dart protoc plugin.
-pub global activate protoc_plugin "$PROTOC_PLUGIN_VERSION"
+pub global activate protoc_plugin $PROTOC_PLUGIN_VERSION
