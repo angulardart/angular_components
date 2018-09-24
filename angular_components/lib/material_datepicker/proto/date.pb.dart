8bcec64a1829afdeb4fa5d2021fe6615585bb901
nshahan <nshahan@google.com>
2018-09-24 09:41:14 -0700

    Switch to building dart proto files externally.
    These files are packaged with the external release of angular_components and should be generated with an external version of the protobuf packages.
    
    - Add test (only run externally on Travis) that will fail if the dart proto files need
      to be re-generated.
    
    PiperOrigin-RevId: 214280643

--- angular_components/lib/material_datepicker/proto/date.pb.dart
+++ angular_components/lib/material_datepicker/proto/date.pb.dart
@@ -1,43 +1,45 @@
 ///
 //  Generated code. Do not modify.
+//  source: date.proto
 ///
-// ignore_for_file: non_constant_identifier_names,library_prefixes
-library angular_components.material_datepicker_date;
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
 
 // ignore: UNUSED_SHOWN_NAME
 import 'dart:core' show int, bool, double, String, List, override;
 
-import 'package:protobuf/protobuf.dart';
+import 'package:protobuf/protobuf.dart' as $pb;
 
-class Date extends GeneratedMessage {
-  static final BuilderInfo _i = new BuilderInfo('Date')
-    ..a<int>(1, 'year', PbFieldType.O3)
-    ..a<int>(2, 'month', PbFieldType.O3)
-    ..a<int>(3, 'day', PbFieldType.O3)
+class Date extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = new $pb.BuilderInfo('Date',
+      package: const $pb.PackageName(
+          'angular_components.lib.material_datepicker.proto'))
+    ..a<int>(1, 'year', $pb.PbFieldType.O3)
+    ..a<int>(2, 'month', $pb.PbFieldType.O3)
+    ..a<int>(3, 'day', $pb.PbFieldType.O3)
     ..hasRequiredFields = false;
 
   Date() : super();
-  Date.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+  Date.fromBuffer(List<int> i,
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromBuffer(i, r);
-  Date.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY])
+  Date.fromJson(String i,
+      [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY])
       : super.fromJson(i, r);
   Date clone() => new Date()..mergeFromMessage(this);
-  BuilderInfo get info_ => _i;
+  Date copyWith(void Function(Date) updates) =>
+      super.copyWith((message) => updates(message as Date));
+  $pb.BuilderInfo get info_ => _i;
   static Date create() => new Date();
-  static PbList<Date> createRepeated() => new PbList<Date>();
-  static Date getDefault() {
-    if (_defaultInstance == null) _defaultInstance = new _ReadonlyDate();
-    return _defaultInstance;
-  }
-
+  static $pb.PbList<Date> createRepeated() => new $pb.PbList<Date>();
+  static Date getDefault() => _defaultInstance ??= create()..freeze();
   static Date _defaultInstance;
   static void $checkItem(Date v) {
-    if (v is! Date) checkItemFailed(v, 'Date');
+    if (v is! Date) $pb.checkItemFailed(v, _i.messageName);
   }
 
   int get year => $_get(0, 0);
   set year(int v) {
-    $_setUnsignedInt32(0, v);
+    $_setSignedInt32(0, v);
   }
 
   bool hasYear() => $_has(0);
@@ -45,7 +47,7 @@ class Date extends GeneratedMessage {
 
   int get month => $_get(1, 0);
   set month(int v) {
-    $_setUnsignedInt32(1, v);
+    $_setSignedInt32(1, v);
   }
 
   bool hasMonth() => $_has(1);
@@ -53,11 +55,9 @@ class Date extends GeneratedMessage {
 
   int get day => $_get(2, 0);
   set day(int v) {
-    $_setUnsignedInt32(2, v);
+    $_setSignedInt32(2, v);
   }
 
   bool hasDay() => $_has(2);
   void clearDay() => clearField(3);
 }
-
-class _ReadonlyDate extends Date with ReadonlyMessageMixin {}
