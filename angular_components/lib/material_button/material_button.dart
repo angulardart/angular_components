2007a1b05c349a1381c3bad0b2f1613a6aeb1757
tsander <tsander@google.com>
2018-09-25 14:41:37 -0700

    Mark certain fields as visibleForTemplate and drop the copied value where not needed.
    PiperOrigin-RevId: 214506779

--- angular_components/lib/material_button/material_button.dart
+++ angular_components/lib/material_button/material_button.dart
@@ -5,6 +5,7 @@
 import 'dart:html';
 
 import 'package:angular/angular.dart';
+import 'package:angular/meta.dart';
 import 'package:angular_components/button_decorator/button_decorator.dart';
 import 'package:angular_components/interfaces/has_disabled.dart';
 import 'package:angular_components/material_ripple/material_ripple.dart';
@@ -101,21 +102,25 @@ class MaterialButtonComponent extends MaterialButtonBase {
   }
 
   @HostBinding('attr.disabled')
+  @visibleForTemplate
   String get hostDisabled => disabled ? "" : null;
 
   @HostBinding('attr.raised')
+  @visibleForTemplate
   String get hostRaised => raised ? "" : null;
 
   @HostBinding('class.is-focused')
+  @visibleForTemplate
   bool get hostClassIsFocused => visualFocus;
 
   @HostBinding('attr.elevation')
+  @visibleForTemplate
   String get hostElevation => '$zElevation';
 
   @HostBinding('attr.animated')
+  @visibleForTemplate
   static const String hostAnimated = 'true';
 
   // not used for M1
-  final bool iconButton = false;
   final String ariaLabel = null;
 }
