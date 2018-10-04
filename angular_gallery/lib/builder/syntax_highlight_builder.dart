6f3c9589c49185b5bedff2eede24e600dc48d918
nshahan <nshahan@google.com>
2018-10-04 13:31:18 -0700

    Automated g4 rollback of changelist 215659502.
    *** Reason for rollback ***
    
    Roll forward with fix for the encoding issue when deployed to appengine.
    Needed to set the charset to "UTF-8".
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 215605010.
    
    *** Reason for rollback ***
    
    Deployed version has a runtime error on pages that need syntax highlighting.
    Rolling back while investigating.
    
    *** Original change description ***
    
    Fix syntax highlighting of code in documentation displayed in the gallery.
    
    Calls out to the js library highlight.js (same library used by Dartdoc) to add styling on the pre elements in documentation only.
    
    https://github.com/highlightjs/highlight.js
    
    ***
    
    PiperOrigin-RevId: 215793400

---
+++ angular_gallery/lib/builder/syntax_highlight_builder.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:build/build.dart';
+
+import '../src/template_util.dart';
+
+/// Generates web/syntax_highlight.scss that contains styles targeting classes
+/// for use with highlight.js. https://highlightjs.org
+class SyntaxHighlightBuilder extends Builder {
+  @override
+  Future build(BuildStep buildStep) async {
+    final newAssetId =
+        AssetId(buildStep.inputId.package, 'web/syntax_highlight.scss');
+    await writeAsset(buildStep,
+        'lib/builder/template/syntax_highlight.scss.mustache', {}, newAssetId);
+  }
+
+  @override
+  Map<String, List<String>> get buildExtensions => {
+        r'$web$': ['syntax_highlight.scss']
+      };
+}
