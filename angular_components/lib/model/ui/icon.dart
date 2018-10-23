20552efbab697efa28fc2267bb94dfec24d8706f
Googler <noreply@google.com>
2018-10-22 19:07:18 -0700

    Create a constructor for Icon that denotes an SVG icon with an empty string as the icon "name".
    PiperOrigin-RevId: 218273608

--- angular_components/lib/model/ui/icon.dart
+++ angular_components/lib/model/ui/icon.dart
@@ -19,4 +19,15 @@ class Icon {
   final String name;
 
   const Icon(this.name);
+
+  /// This constructor can be used when creating an SVG icon.
+  /// Usage instructions: Set the 'icon' in the component you are using to
+  /// this blank icon. Use the following svg-icon mixin to render your svg
+  /// file as an icon:
+  /// http://google3/third_party/dart_src/acx/material_icon/lib/_mixins.scss?l=45
+  ///
+  /// The icon name is set to an empty string because the material-icon
+  /// styling must be preserved in order to properly render the SVG as a
+  /// background-image in place of the material font icon.
+  const Icon.blank() : name = '';
 }
