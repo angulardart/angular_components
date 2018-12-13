4b8a1cd71be0dd9d81f244a3c7ef57698c64ab43
hcameron <hcameron@google.com>
2018-12-13 05:51:26 -0800

    Part of a sequence of CLs refactoring css/material into separate, smaller libraries
    PiperOrigin-RevId: 225357716

--- angular_components/lib/utils/color/material.dart
+++ angular_components/lib/utils/color/material.dart
@@ -46,7 +46,7 @@ const borderDark = Color.rgba(255, 255, 255, dividerOpacity);
 const borderDottedLight = lightestTransparentBlack;
 
 // Text colors.
-// https://material.io/guidelines/style/color.html#color-usability
+// https://material.io/design/color
 const textPrimary = transparentBlack;
 const textSecondary = lightTransparentBlack;
 const textHint = lighterTransparentBlack;
