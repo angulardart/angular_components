4f2416add2659323ed7d514832577908acf5c9a8
nshahan <nshahan@google.com>
2019-01-25 16:52:11 -0800

    Add import prefixes to generated files to avoid unknown identifier errors from the analyzer
    PiperOrigin-RevId: 230994150

--- angular_components/lib/material_menu/affix/caption_affix_model.dart
+++ angular_components/lib/material_menu/affix/caption_affix_model.dart
@@ -4,7 +4,8 @@
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/material_menu/affix/base_affix.dart';
-import 'package:angular_components/material_menu/affix/caption_affix.template.dart';
+import 'package:angular_components/material_menu/affix/caption_affix.template.dart'
+    as ng;
 import 'package:angular_components/model/menu/menu_item_affix.dart';
 
 /// Affix containing text.
@@ -19,7 +20,7 @@ class CaptionAffix extends BaseMenuItemAffixModel {
 
   @override
   ComponentFactory<BaseAffixComponent> get componentFactory =>
-      CaptionAffixComponentNgFactory;
+      ng.CaptionAffixComponentNgFactory;
 
   const CaptionAffix(
       {this.text, this.visibility = IconVisibility.visible, this.cssClass});
