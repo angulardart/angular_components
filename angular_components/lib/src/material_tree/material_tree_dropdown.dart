160612068e444014634a4c980f18939057018c5e
ruka <ruka@google.com>
2019-02-14 08:29:14 -0800

    Expose 'allowParentSingleSelection' in MaterialTreeDropdownComponent
    PiperOrigin-RevId: 233960898

--- angular_components/lib/src/material_tree/material_tree_dropdown.dart
+++ angular_components/lib/src/material_tree/material_tree_dropdown.dart
@@ -101,6 +101,15 @@ class MaterialTreeDropdownComponent extends SelectionContainer
   @Input()
   bool shouldExpandAllWhenFiltered = true;
 
+  /// Whether the widgets supports the selection of non-leaf nodes
+  ///
+  /// When `false`, and the widget is using a single selection model clicking
+  /// the widget should toggle expansion when a non-leaf node is clicked. When
+  /// `true` the widget should select non-leaf nodes when clicked and only
+  /// toggle expansion when the expansion icon is clicked.
+  @Input()
+  bool allowParentSingleSelection = false;
+
   /// Function to convert the selected value to a string to be displayed as the
   /// button text.
   @Input()
