99ac706cfb2a260fc60341c35acb33d9a633270a
cpelling <cpelling@google.com>
2019-03-25 08:03:37 -0700

    Add special case for pt-BR date range formatting to remove repetitive "de"s when the endpoints fall into different years, which are hard to scan.
    - Different years: "26 de fev de 2002 a 13 de mar de 2019" -> "26 fev 2002 a 13 mar 2019"
    
    Unchanged:
    
    - Single date: "13 de mar de 2019"
    - Within same month: "6 a 12 mar 2019"
    - Between different months of the same year: "11 fev a 12 mar 2019"
    
    PiperOrigin-RevId: 240139776

--- angular_components/lib/model/date/date_formatter.dart
+++ angular_components/lib/model/date/date_formatter.dart
@@ -5,37 +5,46 @@
 import 'package:intl/intl.dart';
 import 'package:angular_components/model/date/date.dart';
 
-final _dayFmt = DateFormat.d();
-final _dateFmt = DateFormat.yMMMd();
-final _yearFmt = DateFormat.y();
-final _monthFmt = DateFormat.MMM();
+final _dayFormat = DateFormat.d();
+final _dateFormat = DateFormat.yMMMd();
+final _yearFormat = DateFormat.y();
+final _monthFormat = DateFormat.MMM();
+
+/// A more succinct format for date range endpoints in pt-BR.
+final _ptBrEndpointFormat = DateFormat("d MMM y");
+
+DateFormat _rangeEndpointFormat(String locale) =>
+    locale == 'pt_BR' ? _ptBrEndpointFormat : _dateFormat;
 
 /// Formats the date, or returns '' if the date is null.
-String formatDate(Date date) => date?.format(_dateFmt) ?? '';
+String formatDate(Date date, [DateFormat format]) =>
+    date?.format(format ?? _dateFormat) ?? '';
 String formatRange(DateRange range) {
   if (range == null) return '';
   if (range.start == null && range.end == null)
     return _DateFormatterMessages._allTimeMsg;
-  if (range.start == range.end) return formatDate(range.start);
+  if (range.start == range.end) return formatDate(range.start, _dateFormat);
 
-  if ((range.start == null || range.end == null) ||
+  if (range.start == null ||
+      range.end == null ||
       range.start.year != range.end.year) {
+    final format = _rangeEndpointFormat(Intl.defaultLocale);
     return _DateFormatterMessages._formatArbitraryRange(
-        formatDate(range.start), formatDate(range.end));
+        formatDate(range.start, format), formatDate(range.end, format));
   }
   if (range.start.month != range.end.month) {
     return _DateFormatterMessages._formatSameYearRange(
-        range.start.format(_monthFmt),
-        range.start.format(_dayFmt),
-        range.end.format(_monthFmt),
-        range.end.format(_dayFmt),
-        range.start.format(_yearFmt));
+        range.start.format(_monthFormat),
+        range.start.format(_dayFormat),
+        range.end.format(_monthFormat),
+        range.end.format(_dayFormat),
+        range.start.format(_yearFormat));
   }
   return _DateFormatterMessages._formatSameMonthRange(
-      range.start.format(_monthFmt),
-      range.start.format(_dayFmt),
-      range.end.format(_dayFmt),
-      range.start.format(_yearFmt));
+      range.start.format(_monthFormat),
+      range.start.format(_dayFormat),
+      range.end.format(_dayFormat),
+      range.start.format(_yearFormat));
 }
 
 class _DateFormatterMessages {
