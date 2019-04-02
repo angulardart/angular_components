d4430d61d479ea6ff06037a34fb6dfb3cebce949
Googler <noreply@google.com>
2019-04-02 12:08:24 -0700

    Do not double trigger the callback for a button when the space key is entered.
    It appears that both the host and button were triggering on space keypresses.
    
    PiperOrigin-RevId: 241576775

--- angular_components/lib/button_decorator/button_decorator.dart
+++ angular_components/lib/button_decorator/button_decorator.dart
@@ -35,14 +35,16 @@ class ButtonDirective extends RootFocusable
 
   String _hostTabIndex = '0';
   final String _nonTabbableIndex;
+  bool _shouldHandleSpaceKey;
 
   ButtonDirective(Element element, @Attribute('role') String role,
-      {bool removeTabIndexNonTabbable = false})
+      {bool removeTabIndexNonTabbable = false, bool handleSpacePresses = true})
       : this.role = (role ?? 'button'),
         // Allow the subclass to define how the element should be made
         // untabbable.
         // TODO(google): Consider making this the default for all components.
         _nonTabbableIndex = removeTabIndexNonTabbable ? null : '-1',
+        _shouldHandleSpaceKey = handleSpacePresses ?? true,
         super(element);
 
   /// Role of this component used for a11y.
@@ -87,6 +89,7 @@ class ButtonDirective extends RootFocusable
   @HostListener('keypress')
   void handleKeyPress(KeyboardEvent keyboardEvent) {
     if (disabled) return;
+    if (isSpaceKey(keyboardEvent) && !_shouldHandleSpaceKey) return;
     int keyCode = keyboardEvent.keyCode;
     if (keyCode == KeyCode.ENTER || isSpaceKey(keyboardEvent)) {
       _trigger.add(keyboardEvent);
