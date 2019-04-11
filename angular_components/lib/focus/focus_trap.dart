17dad7ef5f408b3cafa628fc6ffffc5cbc583392
tsander <tsander@google.com>
2019-04-10 18:21:10 -0700

    rollback of changelist 241965449.
    *** Reason for rollback ***
    
    Found a different way to preform the animation without needing this functionality. Rolling back to reduce the maintenance burden of the feature until we find a reason without a better work around.
    
    *** Original change description ***
    
    Adding isActive flag to focus trap.
    
    Focus Trap needs a property to determine if it is active or not when dealing with elements that aren't or can't be hidden.
    
    ***
    
    PiperOrigin-RevId: 242981425

--- angular_components/lib/focus/focus_trap.dart
+++ angular_components/lib/focus/focus_trap.dart
@@ -37,10 +37,6 @@ class FocusTrapComponent implements OnDestroy {
     }
   }
 
-  /// Whether or not the focus trap is active
-  @Input()
-  bool isActive = true;
-
   @override
   void ngOnDestroy() {
     _disposer.dispose();
