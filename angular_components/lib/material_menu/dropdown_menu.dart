aeca642c8af4ff10cad3c6905766939bcca78603
Googler <noreply@google.com>
2019-05-16 06:02:13 -0700

    Add popupClass and buttonAriaLabelledBy inputs to the DropdownMenuComponent.
    PiperOrigin-RevId: 248511961

--- angular_components/lib/material_menu/dropdown_menu.dart
+++ angular_components/lib/material_menu/dropdown_menu.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 import 'package:angular_components/focus/focus.dart';
 import 'package:angular_components/interfaces/has_disabled.dart';
@@ -11,6 +13,7 @@ import 'package:angular_components/material_popup/material_popup.dart';
 import 'package:angular_components/material_select/dropdown_button.dart';
 import 'package:angular_components/mixins/focusable_mixin.dart';
 import 'package:angular_components/model/a11y/keyboard_handler_mixin.dart';
+import 'package:angular_components/utils/angular/css/css.dart';
 import 'package:angular_components/utils/disposer/disposer.dart';
 
 /// The [DropdownMenuComponent] combines a [DropdownButtonComponent] with a
@@ -35,9 +38,10 @@ class DropdownMenuComponent extends Object
         MenuPopupWrapper,
         MenuPopupTrigger
     implements AfterViewInit, HasDisabled, OnDestroy {
+  final HtmlElement _root;
   final _disposer = Disposer.oneShot();
 
-  DropdownMenuComponent(ChangeDetectorRef _changeDetector) {
+  DropdownMenuComponent(ChangeDetectorRef _changeDetector, this._root) {
     // Let Angular pick up changes to [isExpanded] in [MenuPopupWrapper] when
     // it's toggled programmatically, e.g. TabMenuComponent.
     _disposer.addStreamSubscription(isExpandedChange.listen((_) {
@@ -70,13 +74,30 @@ class DropdownMenuComponent extends Object
     focusable = _focusTarget;
   }
 
-  /// Id of element which describes the button for the drop down.
+  /// The id of an element that describes the button for the drop down.
+  @Input()
+  String buttonAriaLabelledBy;
+
+  /// One or more space-delimited ids of elements that provide additional
+  /// description for the button for the drop down.
   @Input()
   String buttonAriaDescribedBy;
 
   /// Whether the menu is tabbable or not.
   @Input()
   bool tabbable = true;
+
+  String _popupClass;
+  String get popupClass => _popupClass;
+
+  /// CSS classes to append onto the menu popup.
+  ///
+  /// These CSS classes will be copied into the popup overlay. The classes can
+  /// be used to select DOM elements within the overlay when the popup is open.
+  @Input()
+  set popupClass(String className) {
+    _popupClass = constructEncapsulatedCss(className, _root.classes);
+  }
 
   bool get dropdownStyle => _dropdownStyle;
   bool _dropdownStyle = false;
