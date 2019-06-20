3fdb66942286c298d4302c3a23566e9a4558f9ef
tsander <tsander@google.com>
2019-06-20 01:30:04 -0700

    Refactor material input to hoist common classes.
    PiperOrigin-RevId: 254153304

--- angular_components/lib/material_input/base_material_input.dart
+++ angular_components/lib/material_input/base_material_input.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'dart:html';
 
 import 'package:angular/angular.dart';
+import 'package:angular/meta.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:intl/intl.dart';
 import 'package:quiver/strings.dart' show isEmpty, isNotEmpty;
@@ -13,6 +14,8 @@ import 'package:angular_components/interfaces/has_disabled.dart';
 import 'package:angular_components/focus/focus.dart';
 import 'package:angular_components/forms/error_renderer.dart' show ErrorFn;
 import 'package:angular_components/mixins/focusable_mixin.dart';
+import 'package:angular_components/utils/angular/properties/properties.dart';
+import 'package:angular_components/utils/angular/reference/reference.dart';
 import 'package:angular_components/utils/disposer/disposer.dart';
 import 'package:angular_components/utils/id_generator/id_generator.dart';
 
@@ -541,4 +544,199 @@ class BaseMaterialInput extends FocusableMixin
 
   static final defaultEmptyMessage = Intl.message('Enter a value',
       desc: 'Error message when the input is empty and required.');
+}
+
+/// Base single line component.
+// This is for GM migration so that the code can be shared. !g3-only
+class BaseSingleLineInputComponent extends BaseMaterialInput
+    implements Focusable, ReferenceDirective, AfterViewInit, OnDestroy {
+  final ChangeDetectorRef _changeDetector;
+
+  @ViewChild('inputEl')
+  ElementRef inputEl;
+
+  @ViewChild('popupSourceEl')
+  ElementRef popupSourceEl;
+
+  /// Container element for popup positioning.
+  @override
+  ElementRef get elementRef => popupSourceEl;
+
+  /// The underlying <input> element.
+  ///
+  /// If you find the need to use this element in application code, you
+  /// may be building new functionality that all ACX users could benefit
+  /// from! If that's the case, please consider contributing your changes
+  /// back upstream. Feel free to contact acx-widgets@ for more guidance.
+  @override
+  ElementRef get inputRef => inputEl;
+
+  /// Type of input.
+  ///
+  /// It can be one of the following:
+  /// {"text", "email", "password", "url", "number", "tel", "search"}
+  String type;
+
+  /// Whether the user can enter multiple values, separated by commas.
+  ///
+  /// Only applies when type = "email", otherwise it is ignored.
+  bool multiple = false;
+
+  final _labelId = SequentialIdGenerator.fromUUID().nextId();
+
+  String get labelId => inputAriaLabel != null ? null : _labelId;
+
+  // Overriden to add a HostListener event.
+  @HostListener('focus')
+  @override
+  void focus() => super.focus();
+
+  /// Input element tabindex.
+  ///
+  /// Used to prevent focus/blur events on disabled inputs that caused weird
+  /// behavior of floating label, input validations, etc.
+  int get inputTabIndex => disabled ? -1 : 0;
+
+  bool get hasLeadingText => isNotEmpty(leadingText);
+  String get leadingText => _leadingText;
+  String _leadingText;
+
+  /// Any text to show at the leading edge of the input -- e.g. a currency
+  /// symbol or similar.
+  @Input()
+  set leadingText(String value) {
+    _leadingText = value;
+    // Possibly set by a directive and not a template. So default change
+    // detection doesn't work without calling markForCheck.
+    _changeDetector.markForCheck();
+  }
+
+  /// Any symbol to show at the leading edge of the input -- e.g. a URL link
+  /// icon or similar.
+  @Input()
+  String leadingGlyph;
+  bool get hasLeadingGlyph => isNotEmpty(leadingGlyph);
+
+  String get trailingText => _trailingText;
+  String _trailingText;
+  bool get hasTrailingText => isNotEmpty(trailingText);
+
+  /// Any text to show at the trailing edge of the input -- e.g. a currency
+  /// symbol or similar.
+  @Input()
+  set trailingText(String value) {
+    _trailingText = value;
+    // Possibly set by a directive and not a template. So default change
+    // detection doesn't work without calling markForCheck.
+    _changeDetector.markForCheck();
+  }
+
+  /// Any symbol to show at the trailing edge of the input -- e.g. a URL link
+  /// icon or similar.
+  @Input()
+  String trailingGlyph;
+  bool get hasTrailingGlyph => isNotEmpty(trailingGlyph);
+
+  /// Aria label used for the trailing glyph.
+  @Input()
+  String trailingGlyphAriaLabel;
+
+  /// Aria label used for the leading glyph.
+  @Input()
+  String leadingGlyphAriaLabel;
+
+  /// The role to assign to the inner input element.
+  ///
+  /// For example, "textbox", "checkbox" and etc.
+  @Input()
+  String role;
+
+  bool get rightAlign => _rightAlign;
+  bool _rightAlign = false;
+
+  /// Whether the input contents should be always right aligned.
+  ///
+  /// Default value is `false`.
+  @Input()
+  set rightAlign(bool value) {
+    _rightAlign = value;
+    // Possibly set by a directive and not a template. So default change
+    // detection doesn't work without calling markForCheck.
+    _changeDetector.markForCheck();
+  }
+
+  /// The ID of an element which should be assigned to the inner input element's
+  /// aria-owns attribute.
+  @Input()
+  String inputAriaOwns;
+
+  /// The ID of an element which should be assigned to the inner input element's
+  /// aria-activedescendant attribute.
+  @Input()
+  String inputAriaActivedescendent;
+
+  /// The value for the input element's aria-haspopup attribute, indicating that
+  /// the element referred to by inputAriaOwns is expandable.
+  ///
+  /// If the element referred to by [inputAriaOwns] is expandable, this should
+  /// be either "true" or the role of the owned element.
+  @Input()
+  String inputAriaHasPopup;
+
+  /// Whether or not the expandable element referred to by [inputAriaOwns] is
+  /// currently visible.
+  @Input()
+  bool inputAriaExpanded;
+
+  /// The autocomplete method applied to the inner input element.
+  ///
+  /// This can be used in conjunction with [inputRole] values of "combobox" or
+  /// "textbox". If this is "list" or "both", [inputAriaHasPopup] should be
+  /// set to "true".
+  @Input()
+  String inputAriaAutocomplete;
+
+  /// The ID of an element which should be assigned to the inner input element's
+  /// aria-controls attribute.
+  @Input()
+  String inputAriaControls;
+
+  BaseSingleLineInputComponent(String type, String multiple, NgControl cd,
+      this._changeDetector, DeferredValidator validator)
+      : super(cd, _changeDetector, validator) {
+    if (type == null) {
+      this.type = 'text';
+    } else if (const ['number', 'tel'].contains(type)) {
+      // For number and telephone, the browser-default validation has to be
+      // locale-aware and/or format-aware. Using 'text' here, so that we
+      // don't use (broken) browser-default validation. Users can still
+      // provide custom validation for these.
+      this.type = 'text';
+    } else {
+      this.type = type;
+    }
+    this.multiple = attributeToBool(multiple);
+  }
+
+  bool get numeric => type == 'number';
+
+  @override
+  bool get labelVisible => !(numeric && invalid) && super.labelVisible;
+
+  @override
+  void ngOnDestroy() {
+    super.ngOnDestroy();
+    inputEl = null;
+    popupSourceEl = null;
+  }
+
+  @visibleForTemplate
+  void handleChange(Event event, InputElement element) {
+    inputChange(
+      element.value,
+      element.validity.valid,
+      element.validationMessage,
+    );
+    event.stopPropagation();
+  }
 }
